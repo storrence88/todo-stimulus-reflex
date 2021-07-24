9e495922ad896c7f21a09f90025489c03e9d3ec7
Steven Torrence <storrence88@gmail.com>
2021-07-24 18:31:27 -0500

    Using server-side callback to set task in task_reflex.rb

--- app/reflexes/task_reflex.rb
+++ app/reflexes/task_reflex.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class TaskReflex < StimulusReflex::Reflex
+  before_reflex :find_task
+
   def toggle
-    Task.find(element.dataset.id).update(completed_at: element.checked ? Time.current : nil)
+    @task.update(completed_at: element.checked ? Time.current : nil)
   end
 
   def destroy
-    Task.find(element.dataset.id).destroy
+    @task.destroy
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(element.dataset.id)
   end
 end
