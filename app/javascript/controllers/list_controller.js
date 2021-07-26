989da3b3057750b9ed23f229cbfd76cc25a1a65e
Steven Torrence <storrence88@gmail.com>
2021-07-25 19:34:35 -0500

    Cleaning up the task editing UX

--- app/javascript/controllers/list_controller.js
+++ app/javascript/controllers/list_controller.js
@@ -8,7 +8,7 @@ export default class extends Controller {
   connect() {
     StimulusReflex.register(this);
     Sortable.create(this.tasksTarget, {
-      filter: '.completed',
+      handle: '.incomplete',
       onEnd: (event) => this.reorder(event)
     });
   }
