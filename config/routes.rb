cdad1c39539344931ee7d82a0264b898fad64ab0
Steven Torrence <storrence88@gmail.com>
2021-07-28 00:06:22 -0500

    Add comments model and associations, create show task show page,

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/show'
   devise_for :users
   root 'lists#index'
-  resources :lists, only: %i[new create destroy]
+  resources :lists, only: %i[new create destroy] do
+    resources :tasks, only: :show
+  end
 end
