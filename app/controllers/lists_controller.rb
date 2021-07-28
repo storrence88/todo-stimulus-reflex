2b0ade6798b14b8c7b711c0ca924b2076970338e
Steven Torrence <storrence88@gmail.com>
2021-07-27 23:20:19 -0500

    Add logic for creating multiple lists at once

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,7 +21,7 @@ class ListsController < ApplicationController
 
     if @list.save
       cable_ready[ListsChannel].outer_html(
-        selector: '#new-list',
+        selector: "#list-#{params.dig(:list, :client_id)}",
         html: render_to_string(@list, assigns: { new_task: Task.new })
       )
     else
--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,8 @@
 class List < ApplicationRecord
   has_many :tasks, ->{ incomplete_first.order(position: :asc) }, dependent: :destroy
   validates :name, presence: true
+
+  def client_id
+    @client_id ||= SecureRandom.uuid
+  end
 end
