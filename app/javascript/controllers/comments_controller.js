9fc65e5427ee0e5db2bc8c836dc3d997b8cc2c3f
Steven Torrence <storrence88@gmail.com>
2021-07-29 23:21:18 -0500

    Refactor tasks channel to use a stimulus controller

---
+++ app/javascript/controllers/comments_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from 'stimulus';
+import consumer from '../channels/consumer';
+import CableReady from 'cable_ready';
+
+export default class extends Controller {
+  connect() {
+    consumer.subscriptions.create(
+      { channel: 'TaskChannel', task_id: this.data.get('taskId') },
+      {
+        connected() {
+          // Called when the subscription is ready for use on the server
+        },
+
+        disconnected() {
+          // Called when the subscription has been terminated by the server
+        },
+
+        received(data) {
+          if (data.cableReady) CableReady.perform(data.operations);
+        }
+      }
+    );
+  }
+}
