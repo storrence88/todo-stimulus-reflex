d4545b657862f139ec8a302814adf99e51ac988f
Steven Torrence <storrence88@gmail.com>
2021-08-04 22:01:41 -0500

    Create spec for task_reflex

--- spec/reflexes/task_reflex_spec.rb
+++ spec/reflexes/task_reflex_spec.rb
@@ -17,6 +17,20 @@ RSpec.describe TaskReflex, type: :reflex do
         reflex.element.checked = true
 
         expect { subject }.to change { task.reload.complete? }.from(false).to(true)
+        expect(task.completer).to eq(user)
+        expect(task.completed_at).to be_present
+      end
+    end
+
+    context 'element is unchecked' do
+      let(:task) { FactoryBot.create :task, :complete }
+
+      it 'incompletes the task' do
+        reflex.element.dataset.id = task.id
+        reflex.element.checked = false
+
+        expect { subject }.to change { task.reload.complete? }.from(true).to(false)
+        expect(task.completed_at).to be_nil
       end
     end
   end
